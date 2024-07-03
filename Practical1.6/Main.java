9c069dc4b6360448bf02978bb5566f1717cde651
Jagdish2401 <149369569+Jagdish2401@users.noreply.github.com>
2024-07-03 15:45:15 +0530

    Add files via upload

---
+++ Practical1.6/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter your number");
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int revrse= 0;
+        while (number!=0) {
+            int digit = number%10;
+             revrse = (revrse*10)+digit;
+             number=number/10;
+        }
+        System.out.println("this is revrse number"+revrse);
+        
+    }
+    
+}
